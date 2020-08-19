8f22b976fcf35a5cedc3e0bbc8ad11db37ba9c6b
sunbin <859990843@qq.com>
2020-08-19 10:52:14 +0800

    优化

--- main.js
+++ main.js
@@ -1,72 +1,67 @@
-let html = document.querySelector("#html");
-let style = document.querySelector("#style");
-
-let string = `/* 你好，
- * 演示一下太极的制作过程
- * 首先准备一个div
- **/
-#div1{
-    border: 1px solid red;
-    width: 400px;
-    height: 400px;
+let div = document.querySelector("p");
+let style = document.querySelector("style");
+let string = `/*大家好,我叫一路人.
+今天我给大家展示一下我的前端功底.
+首先制作一个div.
+*/
+.box{
+    width: 300px;
+    height: 300px;
 }
-/* 接下来把 div变成一个圆
- **/
-#div1{
-  border-radius: 50%;
-  box-shadow: 0 0 3px rgba(0,0,0,0.5);
-  border: none;
-}
-/* 线性渐变
- * 圆一半黑一半白
- **/
-#div1{
-    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
-}
-/* 径向渐变
-*中间两个小球 
-**/
-#div1::before{
-    width: 200px;
-    height: 200px;
-    top: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    background: #000;
+/**/
+/*
+div变圆
+*/
+.box {
+   
     border-radius: 50%;
-    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);
-}
-#div1::after{
-    width: 200px;
-    height: 200px;
-    bottom: 0;
-    left: 50%;
-    transform: translateX(-50%);
-    background: #fff;
-    border-radius: 50%;
-    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%, rgba(0,0,0,1) 100%);
-}
+    
+  }
+  /*
+  线性渐变.
+  */
+  .box{
+    background: linear-gradient(
+        90deg,
+        rgba(0, 0, 0, 1) 50%,
+        rgba(255, 255, 255, 1) 50%
+      );
+  }
+  /*径向渐变*/
+  .box::after {
+    background: radial-gradient(
+      circle,
+      rgba(255, 255, 255, 1) 20%,
+      rgba(0, 0, 0, 1) 20%
+    );
+  }
+  .box::before {
+    background: radial-gradient(
+      circle,
+      rgba(0, 0, 0, 1) 20%,
+      rgba(255, 255, 255, 1) 20%
+    );
+  }
 `;
-let string2 = "";
-let n = 0;
-
-let step = () => {
-  setTimeout(() => {
-    if (string[n] === "\n") {
-      string2 += "<br>";
-    } else if (string[n] === " ") {
-      string2 += "&nbsp;";
-    } else {
-      string2 += string[n];
-    }
-    html.innerHTML = string2;
-    style.innerHTML = string.substring(0, n);
-    window.scrollTo(0, 99999);
-    html.scrollTo(0, 99999);
-    if (n < string.length - 1) {
-      n += 1;
-      step();
-    }
-  }, 1);
-};
-step(); // 1=>2
+let stringP = string;
+let n = 1;
+let p = 1;
+let x = setInterval(() => {
+  if (p >= stringP.length) {
+    clearInterval(x);
+  }
+  if (stringP[p] === string[12]) {
+    div.innerHTML = stringP.slice(0, p) + "<br>";
+    stringP = stringP.slice(0, p) + "<br>" + stringP.slice(p);
+    p = p + 5;
+    n++;
+  } else {
+    div.innerHTML = stringP.slice(0, p);
+    console.log(p);
+    style.innerHTML = string.slice(0, n);
+    n++;
+    p++;
+  }
+  window.scrollTo(0, 9999);
+  div.scrollTo(0, 9999);
+}, 5);
